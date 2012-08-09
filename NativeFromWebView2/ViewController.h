be028b3ca4b63daf6ac0c2017b9c2a4d23cc51cd
波切賢一 <hakiri.kenichi@adways.net>
2012-08-09 15:48:23 +0900

    Initial Commit

---
+++ NativeFromWebView2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NativeFromWebView2
+//
+//  Created by 波切 賢一 on 12/08/09.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ NativeFromWebView2/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  NativeFromWebView2
+//
+//  Created by 波切 賢一 on 12/08/09.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
